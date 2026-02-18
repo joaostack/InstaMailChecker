36058eaf2c8cca0c75c1499aed8f41ae1cae7fdc
João H. <83355344+joaostack@users.noreply.github.com>
2026-02-18 04:55:39 -0300

    New Version 2.0

--- src/Program.cs
+++ src/Program.cs
@@ -1,79 +1,112 @@
-﻿using System.Text.Json;
+namespace InstaMailChecker;
+
+using Spectre.Console;
+using Spectre.Console.Cli;
+using System.ComponentModel;
 using System.Text.RegularExpressions;
 
-namespace InstaMailChecker
+class Program
 {
-    public class Program
+    static async Task Main(string[] args)
     {
-        private static readonly HttpClient _client = new HttpClient();
-        private static readonly string art = @"
-       /\       
-      /  \      
-     /,--.\     
-    /< () >\    
-   /  `--'  \   
-  /          \  
- /            \ 
-/______________\";
+        var app = new CommandApp<Commands>();
+        await app.RunAsync(args);
+    }
+}
 
-        static async Task Main(string[] args)
+public class Commands : AsyncCommand<Commands.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandOption("-e|--email", isRequired: true)]
+        [Description("Target e-mail")]
+        public required string TargetMail { get; set; }
+    }
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
+    {
+        try
         {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine(art);
-            Console.ResetColor();
+            var targetMail = settings.TargetMail;
+            var httpClient = new HttpClient();
+            var instagram = new Instagram(httpClient);
+            var randomPass = new Random().Next(100000000);
+            var responseStatus = await instagram.CheckMail(targetMail, randomPass.ToString());
 
-            if (args.Length == 0)
+            switch (responseStatus)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Specify target mail!");
-                Console.ResetColor();
-                return;
+                case 200:
+                    AnsiConsole.MarkupLine($"[bold blue][[+]][/] [bold green]E-mail '{targetMail}' is UP![/]");
+                    break;
+                case 429:
+                    AnsiConsole.MarkupLine("[bold blue][[-]][/] [bold red]Error 429 (try use different IP address)[/]");
+                    break;
+                case 404:
+                    AnsiConsole.MarkupLine($"[bold blue][[-]][/] [bold red]E-mail '{targetMail}' is DOWN![/]");
+                    break;
+                case 400:
+                    AnsiConsole.MarkupLine("[bold blue][[-]][/] [bold red]Error 400 CSRF Token not found![/]");
+                    break;
+                case 500:
+                    AnsiConsole.MarkupLine("[bold blue][[-]][/] [bold red]Error 500 on instgram[/]");
+                    break;
             }
-
-            var mail = args[0].Trim();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"[*] Checking '{mail}'...");
-            Console.ResetColor();
-            var hash = await GetHash();
-            await CheckMail(hash, mail, "@joaostack");
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteException(ex);
         }
 
-        public static async Task<string> GetHash()
-        {
-            return Guid.NewGuid().ToString();
-        }
+        return 0;
+    }
+}
 
-        public static async Task CheckMail(string hash, string mail, string testPass)
+public class Instagram
+{
+    private static HttpClient _httpClient { get; set; }
+
+    public Instagram(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+
+        // first headers section
+        _httpClient.DefaultRequestHeaders.Clear();
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+        _httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8");
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9");
+        _httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+
+        // second headers section
+        _httpClient.DefaultRequestHeaders.Clear();
+        _httpClient.DefaultRequestHeaders.Add("Host", "i.instagram.com");
+        _httpClient.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "Instagram 6.12.1 Android (29/10; 480dpi; 1080x2137; HUAWEI/HONOR; JSN-L22; HWJSN-H; kirin710; en_SA)");
+        _httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-SA, en-US");
+        _httpClient.DefaultRequestHeaders.Add("X-IG-Connection-Type", "WIFI");
+        _httpClient.DefaultRequestHeaders.Add("X-IG-Capabilities", "AQ==");
+
+    }
+
+    public async Task<int> CheckMail(string mail, string testPass)
+    {
+        try
         {
-            try
+            // GUIDs
+            var guid = Guid.NewGuid().ToString();
+            var hash = Guid.NewGuid().ToString();
+
+            // Grep CSRF Token
+            var signUp = await _httpClient.GetAsync("https://www.instagram.com/accounts/emailsignup/");
+            var signUpBody = await signUp.Content.ReadAsStringAsync();
+            var pattern = "\"csrf_token\":\"(.*?)\"";
+            Match match = Regex.Match(signUpBody, pattern);
+            var version = Environment.GetEnvironmentVariable("Version") ?? string.Empty;
+            _httpClient.DefaultRequestHeaders.Add("Cookie2", version + "=1");
+
+            if (match.Success)
             {
-                var guid = Guid.NewGuid().ToString();
-                _client.DefaultRequestHeaders.Clear();
-                _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
-                _client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8");
-                _client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9");
-                _client.DefaultRequestHeaders.Add("Connection", "keep-alive");
-
-                var signUp = await _client.GetAsync("https://www.instagram.com/accounts/emailsignup/");
-                var signUpBody = await signUp.Content.ReadAsStringAsync();
-                string pattern = "\"csrf_token\":\"(.*?)\"";
-                Match match = Regex.Match(signUpBody, pattern);
-
-                _client.DefaultRequestHeaders.Clear();
-                _client.DefaultRequestHeaders.Add("Host", "i.instagram.com");
-                _client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
-                _client.DefaultRequestHeaders.Add("User-Agent", "Instagram 6.12.1 Android (29/10; 480dpi; 1080x2137; HUAWEI/HONOR; JSN-L22; HWJSN-H; kirin710; en_SA)");
-                _client.DefaultRequestHeaders.Add("Accept-Language", "en-SA, en-US");
-                _client.DefaultRequestHeaders.Add("X-IG-Connection-Type", "WIFI");
-                _client.DefaultRequestHeaders.Add("X-IG-Capabilities", "AQ==");
-
-                var version = Environment.GetEnvironmentVariable("Version") ?? string.Empty;
-                _client.DefaultRequestHeaders.Add("Cookie2", version + "=1");
-
-                if (match.Success)
-                {
-                    var csrfToken = match.Groups[1].Value;
-                    var data = new Dictionary<string, string>
+                var csrfToken = match.Groups[1].Value;
+                var data = new Dictionary<string, string>
                 {
                     { "username", mail },
                     { "password", testPass },
@@ -82,51 +115,41 @@ namespace InstaMailChecker
                     { "_csrftoken", csrfToken }
                 };
 
-                    var content = new FormUrlEncodedContent(new[]
-                    {
+                var content = new FormUrlEncodedContent(new[]
+                {
                     new KeyValuePair<string, string>("ig_sig_key_version", "4"),
                     new KeyValuePair<string, string>("signed_body", $"{Guid.NewGuid().ToString("N")}.{System.Text.Json.JsonSerializer.Serialize(data)}")
                 });
 
-                    var login = await _client.PostAsync("https://i.instagram.com/api/v1/accounts/login/", content);
-                    var response = await login.Content.ReadAsStringAsync();
+                var login = await _httpClient.PostAsync("https://i.instagram.com/api/v1/accounts/login/", content);
+                var response = await login.Content.ReadAsStringAsync();
 
-                    if (response.Contains("bad_password"))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"[+] REGISTERED => {mail}");
-                    }
-                    else if (response.Contains("invalid_user"))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"[-] NOT REGISTERED => {mail}");
-                    }
-                    else if (response.Contains("Please wait a few minutes before you try again."))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"ERROR: Blocked by instagram, try later!");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Error: {response}");
-                    }
+                if (response.Contains("bad_password"))
+                {
+                    return 200;
+                }
+
+                if (response.Contains("invalid_user"))
+                {
+                    return 404;
+                }
+                else if (response.Contains("Please wait a few minutes before you try again."))
+                {
+                    return 429;
                 }
                 else
                 {
-                    Console.WriteLine("No csrf token found ):");
-                    return;
+                    return 500;
                 }
             }
-            catch (Exception ex)
+            else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"ERROR: {ex.Message}");
+                return 400;
             }
-            finally
-            {
-                Console.ResetColor();
-            }
+        }
+        catch
+        {
+            throw;
         }
     }
 }
